0ff7ee17d61656de4cfa0bc435b32f3a19e238ee
SeYun <ksy642@naver.com>
2025-01-20 00:19:41 +0900

    ScriptableObject를 통해 Item들 관리하는 ItemData만들어둠

---
+++ UnityProjects/UntilTheEnd/Assets/_UntilTheEnd/_Program/Scripts/ScriptableObjects/ItemData.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+namespace UntilTheEnd
+{
+    [CreateAssetMenu(fileName = "ItemData", menuName = "Scriptable Objects/InventoryData")]
+    public class ItemData : ScriptableObject
+    {
+        public List<Item> itemList = new List<Item>();
+
+        // 아이템 수집
+        public void ColletItem(int index)
+        {
+            itemList[index].isCollect = true;
+        }
+    }
+
+    [Serializable]
+    public class Item
+    {
+        public bool isCollect = false; // 수집여부
+
+        public Sprite iconSprite;
+        public string name = "아이템";
+        public string description = "간략한 설명";
+
+        public bool isEquipable = false;   // 장착 가능 여부
+        public bool isConsumable = false; // 소모품 여부
+    }
+}
