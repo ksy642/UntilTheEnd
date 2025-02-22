4e33f198e37d72e7af4b8ef01b186224a13c8e35
SeYun <ksy642@naver.com>
2025-02-22 21:38:28 +0900

    어제 세팅하면서 빠진 로비씬 스크립트 (딱히 내용은 없음)

--- UnityProjects/UntilTheEnd/Assets/_UntilTheEnd/_Program/Scripts/_SceneScripts/LobbySceneScript.cs
+++ UnityProjects/UntilTheEnd/Assets/_UntilTheEnd/_Program/Scripts/_SceneScripts/LobbySceneScript.cs
@@ -2,27 +2,19 @@ using UnityEngine;
 
 namespace UntilTheEnd
 {
+    /// <summary>
+    /// 여기 씬을 들어올 때 세이브 파일이 있는지 먼저 체크를 하고
+    /// , 세이브 파일이 있다면 불러오기 버튼을 활성화시키고
+    /// , 없다면 비활성화로 유지시키는게 좋을듯?
+    /// 
+    /// 새 게임을 누르면 기존에 저장된 파일을 싹다 날리는 식으로 진행하는게 맞나?
+    /// 아니면 새 게임을 해도 저장안되다가 처음 저장되는 포인트에서 싹다 날리는게 맞나? (이게 맞을듯?)
+    /// </summary>
     public class LobbySceneScript : MonoBehaviour
     {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
+        private void Start()
         {
 
         }
-
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
-
-
-
-        //// 테스트용
-        //public void OnClick_ToMainScene()
-        //{
-        //    Debug.LogWarning("클릭됨");
-        //    UIManager.instance.FadeToScene(StringValues.Scene.main);
-        //}
     }
 }
